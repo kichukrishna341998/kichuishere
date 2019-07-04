69343738f0e301c3ab57454414726fd82f19ebda
training_d2.03.07 <training_d2.03.07@DDC2-D-45G2N62.dir.svc.accenture.com>
2019-07-04 11:32:41 +0530

    json is added

--- src/test/java/cucumberkichu2/calrunner2.java
+++ src/test/java/cucumberkichu2/calrunner2.java
@@ -2,8 +2,10 @@ package cucumberkichu2;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin= {"json:cucumber.json"})
 public class calrunner2
 {
 
